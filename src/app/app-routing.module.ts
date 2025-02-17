58d4131f98ba76c6b03263c0f847f16c7b92d7e3
ObertB <obertbalint01@gmail.com>
2025-02-17 16:51:01 +0100

    dolgozat0217

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'parent', component: ParentComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }, // Alapértelmezett útvonal átirányítása
+  { path: '**', redirectTo: 'products' } // Hibás útvonal esetén is a products-ra irányít
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  standalone: false,
   templateUrl: './child.component.html',
-  styleUrl: './child.component.css'
+  styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
+  @Input() num1!: number;
+  @Input() num2!: number;
+  @Output() resultEvent = new EventEmitter<number>();
 
+  calculate(operation: string) {
+    let result: number;
+    switch (operation) {
+      case 'add':
+        result = this.num1 + this.num2;
+        break;
+      case 'subtract':
+        result = this.num1 - this.num2;
+        break;
+      case 'multiply':
+        result = this.num1 * this.num2;
+        break;
+      case 'divide':
+        result = this.num2 !== 0 ? this.num1 / this.num2 : 0;
+        break;
+      default:
+        result = 0;
+    }
+    
+    this.resultEvent.emit(result); // Eredmény visszaküldése a ParentComponent-nek
+  }
 }
